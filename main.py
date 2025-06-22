47d21185027f3c6c9831a892a6f3d52c42d4d816
AvengedMulisha <jwmerts2010@gmail.com>
2025-06-21 21:26:34 -0500

    Cleaned up space readme

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ import discord
 from discord.ext import commands
 import os
 
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot('!', intents=intents)
 
-
-
 @client.event
 async def on_ready():
     await client.load_extension('clean_up_message')
     print(f'We have logged in as {client.user}')
-
-
 
 
 client.run(os.environ['DISCORD_TOKEN'])
